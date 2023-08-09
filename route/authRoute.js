e55b3235f1dd11d8e8d2928e3991cfbc55a96652
Celine Amarachi <ecelineamarachi@yahoo.com>
2023-08-09 12:18:40 +0100

    Update authRoute.js to display welcome message

--- route/authRoute.js
+++ route/authRoute.js
@@ -3,11 +3,12 @@ const express = require("express");
 const {signup, signin} = require("../controller/authController");
 const authRouter = express.Router();
 
+authRouter.get("/", (req, res) => {
+  res.send("Welcome to Blog API");
+});
 
 authRouter.post("/signup", signup);
-// authRouter.get("/signup", signUp);
 authRouter.post("/signin", signin);
-// authRouter.get("/signin", signIn);
 
 
 module.exports = authRouter;
