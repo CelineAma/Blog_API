e25fceb902ce6b081b19bc60b3905086dee8a768
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-10-07 16:05:58 +0100

    feat(logger): log requests to the API
    - disable rate-limit
    - remove mongoose deprecation warnings

--- config/db.js
+++ config/db.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 //To connect the mongodb database
 module.exports = function () {
     const MONGODB_CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+    
+    mongoose.set('strictQuery', true);
+    mongoose.set('strictPopulate', false);
     mongoose.connect(MONGODB_CONNECTION_URL);
     mongoose.connection.on("connected", async () => {
         console.log("Connection to MongoDB is Successful");
